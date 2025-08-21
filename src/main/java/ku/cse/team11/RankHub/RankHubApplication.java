a51cc9fc8b27f2123fe2293a9bdbbe1f3a80c3ad
Saekbit <dtq1014@naver.com>
2025-08-21 17:06:21 +0900

    create project

---
+++ src/main/java/ku/cse/team11/RankHub/RankHubApplication.java
@@ -0,0 +1,13 @@
+package ku.cse.team11.RankHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/cse/team11/RankHub/RankHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cse.team11.RankHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
